7d9106ae0e2c417e346e0605f04839461eb095e8
Boris Churzin <essl.main@gmail.com>
2016-11-19 23:47:32 +0200

    fix

--- temp/Spawner.js
+++ temp/Spawner.js
@@ -18,7 +18,7 @@ function Spawner(room) {
 
 Spawner.prototype.renewNearbyCreeps = function() {
   if(this.spawner !== undefined && !this.spawner.spawning) {
-    var creeps = this.spawner.pos.findInRange(FIND_MY_CREEPS, 1).filter(function(creep) { return creep.hits < config.renew_to_ttl }).sort(function(a, b) { return a.hits > config.critical_ttl ? 1 : -1 });
+    var creeps = this.spawner.pos.findInRange(FIND_MY_CREEPS, 1).filter(function(creep) { return !_.includes(creep.body, MOVE) && creep.hits < config.renew_to_ttl }).sort(function(a, b) { return a.hits > config.critical_ttl ? 1 : -1 });
     if(creeps.length > 0) {
       this.spawner.renewCreep(creeps[0]);
       return true
