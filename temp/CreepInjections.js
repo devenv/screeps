d67084a158f5528bbcea585d3e423550ecef8c04
Boris Churzin <essl.main@gmail.com>
2016-11-19 21:37:00 +0200

    fix

--- temp/CreepInjections.js
+++ temp/CreepInjections.js
@@ -25,7 +25,7 @@ Creep.prototype.renew = function() {
   if(this.pos.isNearTo(spawn)) {
     this.transfer(spawn, RESOURCE_ENERGY);
     spawn.renewCreep(this);
-    if(this.ticksToLive > config.renew_to_ttl || Math.random() < config.stop_renew_prob) {
+    if(this.room.energyAvailable < 400 || this.ticksToLive > config.renew_to_ttl || Math.random() < config.stop_renew_prob) {
       this.memory.mode = undefined;
     }
   } else {
