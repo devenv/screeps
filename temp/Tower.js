42564a325132d7332e28ffd7e859625018384a6e
Boris Churzin <boris.c@matomy.com>
2016-11-20 19:38:01 +0200

    fix

--- temp/Tower.js
+++ temp/Tower.js
@@ -1,29 +1,30 @@
 var config = require('Config');
 
 function Tower(tower) {
-    this.tower = tower;
+  this.tower = tower;
 };
 
 Tower.prototype.act = function() {
-    var targets = this.tower.room.findHostiles();
-    if(targets.length > 0) {
-        this.tower.attack(targets[0]);
+  var targets = this.tower.room.findHostiles();
+  if(targets.length > 0) {
+    console.log(targets[0]);
+    this.tower.attack(targets[0]);
+  } else {
+    var wounded = Object.keys(Game.creeps).map(name => Game.creeps[name]).filter(creep => creep.my && creep.room.name === this.tower.name && creep.hits < creep.hitsMax);
+    if(wounded.length > 0) {
+      this.tower.heal(wounded[0]);
     } else {
-        var wounded = Object.keys(Game.creeps).map(name => Game.creeps[name]).filter(creep => creep.my && creep.room.name === this.tower.name && creep.hits < creep.hitsMax);
-        if(wounded.length > 0) {
-            this.tower.heal(wounded[0]);
-        } else {
-            var broken = Object.keys(Game.structures).map(name => Game.structures[name]).filter(st => st.room.name === this.tower.name && st.hits < st.hitsMax);
-            if(broken.length > 0) {
-                this.tower.repair(broken[0]);
-            } else if(this.tower.energy > this.tower.energyCapacity / 4) {
-                var to_repair = this.tower.room.brokenStructures();
-                if(to_repair.length > 0) {
-                    this.tower.repair(to_repair[0]);
-                }
-            }
+      var broken = Object.keys(Game.structures).map(name => Game.structures[name]).filter(st => st.room.name === this.tower.name && st.hits < st.hitsMax);
+      if(broken.length > 0) {
+        this.tower.repair(broken[0]);
+      } else if(this.tower.energy > this.tower.energyCapacity / 4) {
+        var to_repair = this.tower.room.brokenStructures();
+        if(to_repair.length > 0) {
+          this.tower.repair(to_repair[0]);
         }
+      }
     }
+  }
 };
 
 module.exports = Tower;
