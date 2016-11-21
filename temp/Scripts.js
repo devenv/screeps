31369df51d65c3f1d6935245465babb07515c631
Boris Churzin <boris.c@matomy.com>
2016-11-21 18:00:34 +0200

    fix

--- temp/Scripts.js
+++ temp/Scripts.js
@@ -4,7 +4,7 @@ module.exports = {
     brokenRoads: function() {
         Object.keys(Game.rooms).forEach(roomName => {
            var roads = Game.rooms[roomName].find(FIND_STRUCTURES).filter(st => st.structureType === STRUCTURE_ROAD);
-           var broken = roads.filter(road road.hits < road.hitsMax);
+           var broken = roads.filter(road => road.hits < road.hitsMax);
            var hits = broken.reduce((s, road) => s += road.hits, 0);
            var max = broken.reduce((s, road) => s += road.hitsMax, 0);
            console.log("roads: " + roads.length + ", broken: " + broken.length + ", state: " + (Math.round(hits / max * 100)) + ", to fix: " + (max - hits));
