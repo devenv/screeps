cb7a70b1fa43b08c96ec23e6be2013557565544f
Boris Churzin <essl.main@gmail.com>
2016-11-21 20:17:12 +0200

    update

--- temp/Carrier.js
+++ temp/Carrier.js
@@ -66,7 +66,6 @@ Carrier.prototype.act = function() {
         } else {
           src = Game.getObjectById(this.creep.memory.src);
         }
-        this.creep.withdrawFromNearby();
         if(this.creep.pos.isNearTo(src)) {
           if ((this.creep.room.energyAvailable - src.energy) / this.creep.room.extensions().length > config.min_extension_energy) {
             this.creep.withdraw(src, RESOURCE_ENERGY);
