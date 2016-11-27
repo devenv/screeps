1e2f32b448e17527b6cf4000b29b24b3e628e452
Boris Churzin <essl.main@gmail.com>
2016-11-27 08:09:27 +0200

    update

--- temp/Miner.js
+++ temp/Miner.js
@@ -13,7 +13,7 @@ Miner.prototype.act = function() {
   if(this.creep.memory.mode === 'mining') {
     if(this.creep.memory.source === undefined) {
       this.creep.originRoom().find(FIND_SOURCES).some(source => {
-        var creeps_working = _.values(Game.creeps).filter(creep => creep.memory.source === source.id).length;
+        var creeps_working = _.values(Game.creeps).filter(creep => creep.memory.level >= this.creep.memory.level && creep.memory.source === source.id).length;
         if (creeps_working < this.creep.originRoom().countFreeSpots(source.pos)) {
           this.creep.memory.source = source.id;
           return true;
@@ -23,7 +23,7 @@ Miner.prototype.act = function() {
     if(this.creep.memory.source === undefined) {
       _.values(Game.rooms).filter(room => room.owner === undefined).some(room => {
         return room.find(FIND_SOURCES).some(source => {
-          var creeps_working = _.values(Game.creeps).filter(creep => creep.memory.source === source.id).length;
+          var creeps_working = _.values(Game.creeps).filter(creep => creep.memory.level >= this.creep.memory.level && creep.memory.source === source.id).length;
           if (creeps_working < room.countFreeSpots(source.pos) * _.values(Game.rooms).filter(room => room.controller.my).length) {
             this.creep.memory.source = source.id;
             return true;
@@ -31,6 +31,7 @@ Miner.prototype.act = function() {
         });
       });
     }
+
     if(this.creep.memory.source !== undefined) {
       var source = Game.getObjectById(this.creep.memory.source);
       if(this.creep.pos.isNearTo(source)) {
