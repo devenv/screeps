6c412205226b6885e7ea88025549bee3ec9012ba
Boris Churzin <boris.c@matomy.com>
2016-11-28 19:31:08 +0200

    fix

--- temp/CreepInjections.js
+++ temp/CreepInjections.js
@@ -25,7 +25,7 @@ Creep.prototype.renew = function() {
   var spawn = this.room.spawn();
   if(this.pos.isNearTo(spawn)) {
     this.transfer(spawn, RESOURCE_ENERGY);
-    if(this.room.energyAvailable < config.minimal_energy_for_renew || this.ticksToLive > config.renew_to_ttl || Math.random() < config.stop_renew_prob) {
+    if(!this.room.hasSpareEnergy() || this.ticksToLive > config.renew_to_ttl || Math.random() < config.stop_renew_prob) {
       this.memory.mode = undefined;
     }
   } else {
@@ -45,30 +45,30 @@ Creep.prototype.pickupEnergy = function() {
 Creep.prototype.shouldRenew = function() { return !this.body.some(part => part.type === CLAIM) && this.room.energyAvailable > config.minimal_energy_for_renew && this.ticksToLive < config.renew_ttl && this.memory.level >= this.room.memory.level };
 
 Creep.prototype.goTo = function(pos) {
-    this.memory.moved = true;
-    if(!this.pos.isNearTo(pos)) {
-        var res = this.moveTo(pos, {reusePath: true});
-        // if(res === ERR_TIRED) {
-        //     this.say('tired');
-        // }
-        if(res !== 0 && res !== ERR_TIRED) {
-            res = this.moveTo(pos);
-            if(res !== 0 && res !== ERR_TIRED) {
-                this.say('stuck:' + res);
-                // console.log(pos)
-                if(Math.random() < 0.1) {
-                    this.twitch();
-                }
-            }
-            // } else {
-            // Utils.twitch(creep);
+  this.memory.moved = true;
+  if(!this.pos.isNearTo(pos)) {
+    var res = this.moveTo(pos, {reusePath: true});
+    // if(res === ERR_TIRED) {
+    //     this.say('tired');
+    // }
+    if(res !== 0 && res !== ERR_TIRED) {
+      res = this.moveTo(pos);
+      if(res !== 0 && res !== ERR_TIRED) {
+        this.say('stuck:' + res);
+        // console.log(pos)
+        if(Math.random() < 0.1) {
+          this.twitch();
         }
+      }
+      // } else {
+      // Utils.twitch(creep);
     }
+  }
 }
 
 Creep.prototype.twitch = function() {
-    this.say('twitch');
-    this.move(dirs[Math.floor(Math.random() * dirs.length)]);
+  this.say('twitch');
+  this.move(dirs[Math.floor(Math.random() * dirs.length)]);
 }
 
 Creep.prototype.originRoom = function() {
@@ -76,18 +76,18 @@ Creep.prototype.originRoom = function() {
 }
 
 Creep.prototype.withdrawFromNearby = function() {
-    var containers = this.pos.findInRange(FIND_STRUCTURES, 1, {filter: {structureType: STRUCTURE_CONTAINER}})
-    .sort((a, b) => a.energy > b.energy ? -1 : 1);
-    if(containers !== undefined && containers.length > 0) {
-        this.withdraw(containers[0], RESOURCE_ENERGY);
-    }
+  var containers = this.pos.findInRange(FIND_STRUCTURES, 1, {filter: {structureType: STRUCTURE_CONTAINER}})
+  .sort((a, b) => a.energy > b.energy ? -1 : 1);
+  if(containers !== undefined && containers.length > 0) {
+    this.withdraw(containers[0], RESOURCE_ENERGY);
+  }
 }
 
 Creep.prototype.transferToNearby= function() {
-    var containers = this.pos.findInRange(FIND_STRUCTURES, 1)
-    .filter(st => _.contains(energySinks, st.structureType))
-    .sort((a, b) => a.energy > b.energy ? 1 : -1);
-    if(containers.length > 0) {
-        this.transfer(containers[0], RESOURCE_ENERGY);
-    }
+  var containers = this.pos.findInRange(FIND_STRUCTURES, 1)
+  .filter(st => _.contains(energySinks, st.structureType))
+  .sort((a, b) => a.energy > b.energy ? 1 : -1);
+  if(containers.length > 0) {
+    this.transfer(containers[0], RESOURCE_ENERGY);
+  }
 }
