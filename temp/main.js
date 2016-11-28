eff73ff2a374e63914596d10f1dd1ed00c502b1a
Boris Churzin <essl.main@gmail.com>
2016-11-28 22:03:03 +0200

    update

--- temp/main.js
+++ temp/main.js
@@ -18,9 +18,6 @@ var CreepInjections = require('CreepInjections');
 
 module.exports.loop = function() {
   var cpu = Game.cpu.getUsed();
-  if(Memory.stats === undefined) {
-    Memory.stats = {};
-  }
   var exception;
   try {
     new Flags().process();
