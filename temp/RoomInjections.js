7c889c87718343754b8d4cd5284a2914b81008cd
Boris Churzin <boris.c@matomy.com>
2016-12-01 11:02:04 +0200

    fix

--- temp/RoomInjections.js
+++ temp/RoomInjections.js
@@ -101,5 +101,5 @@ Room.prototype.brokenStructures = function() {
 }
 
 Room.prototype.hasSpareEnergy = function() {
-  return this.energyAvailable > setups.cost('miner', _.max([15, this.level()])) * 1.2;
+  return this.energyAvailable > setups.cost('miner', _.min([15, this.level()])) * 1.2;
 }
