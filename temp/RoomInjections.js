ed79590dfcc82cdf21769ff007e95984609dc9cd
Boris Churzin <boris.c@matomy.com>
2016-12-07 21:40:00 +0200

    fix

--- temp/RoomInjections.js
+++ temp/RoomInjections.js
@@ -40,7 +40,7 @@ Room.prototype.update = function() {
 }
 
 Room.prototype.longUpdate = function() {
-  if(Game.time % 100) {
+  if(Game.time % 100 === 1) {
     Object.keys(this.memory.path_counts).filter(key => this.memory.path_counts[key] <= 1).forEach(key => this.memory.path_counts[key] = undefined);
     Object.keys(this.memory.path_counts).forEach(key => this.memory.path_counts[key] -= 1);
   }
