574b05aa572be36f2080e3d2a242fff37894d7dd
Boris Churzin <boris.c@matomy.com>
2016-12-07 20:12:15 +0200

    fix

--- temp/Tower.js
+++ temp/Tower.js
@@ -7,7 +7,8 @@ function Tower(tower) {
 Tower.prototype.act = function() {
   var targets = this.tower.room.hostileCreeps;
   if(targets.length > 0) {
-    this.tower.attack(targets[0]);
+    var target = this.room.find(FIND_HOSTILE_CREEPS, {filter: t => t.name !== 'Source Keeper'})[0];
+    this.tower.attack(target);
   } else {
     var wounded = Object.keys(Game.creeps).map(name => Game.creeps[name]).filter(creep => creep.my && creep.room.name === this.tower.name && creep.hits < creep.hitsMax);
     if(wounded.length > 0) {
