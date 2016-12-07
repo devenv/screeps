44f9be4b6284e1c72c09dc672d0cf0a760ac8781
Boris Churzin <boris.c@matomy.com>
2016-12-07 19:17:04 +0200

    fix

--- temp/RoomInjections.js
+++ temp/RoomInjections.js
@@ -40,7 +40,7 @@ Room.prototype.update = function() {
       this.modernCreeps[role] = [];
     });
     this.find(FIND_MY_CREEPS).forEach(creep => this.creeps[creep.memory.role].push(creep.name));
-    _.flatten(_.values(this.creeps)).filter(creep => Game.creeps[creep].memory.level >= this.level).forEach(creep => this.modernCreeps[Game.creeps[creep].memory.role].push(creep));
+    _.flatten(_.values(this.creeps)).filter(name => Game.creeps[name].memory.level >= this.level).forEach(name => this.modernCreeps[Game.creeps[name].memory.role].push(name));
   }
 
   this.hostileCreeps = this.find(FIND_HOSTILE_CREEPS, {filter: t => t.name !== 'Source Keeper'}).map(creep => creep.name);
