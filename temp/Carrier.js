c7fa99c98e19254cb9888063e2e7f610017138f3
Boris Churzin <boris.c@matomy.com>
2016-12-12 19:23:16 +0200

    fix

--- temp/Carrier.js
+++ temp/Carrier.js
@@ -14,53 +14,37 @@ Carrier.prototype.act = function() {
   if(Game.time % 50 === 0 && this.creep.memory.supplying) {
     this.creep.say('reset');
     this.creep.memory.supplying = undefined;
-    this.creep.memory.owner = undefined;
     this.creep.memory.target = undefined;
   }
 
   if(this.creep.memory.target === undefined) {
     this.creep.say("?");
-    var sources = this.room.sources().filter(source => !_.values(Game.creeps).some(creep => creep.memory.role === 'carrier' && utils.samePos(creep.memory.owner, source.pos)));
-    if(sources.length > 0) {
-      sources.some(source => {
-        if(this.room.memory.source_containers.length > 0) {
-          this.creep.memory.supplying = false;
-          this.creep.memory.owner = source.pos;
-          this.creep.memory.target = this.room.memory.source_containers[0];
-          return true;
-        }
-      });
+    var containers = this.room.source_containers().filter(container => !_.values(Game.creeps).some(creep => creep.memory.role === 'carrier' && creep.memory.target === container.id));
+    if(containers.length > 0) {
+      this.creep.memory.supplying = false;
+      this.creep.memory.target = this.room.memory.source_containers[0];
     }
     if(Memory.has_cpu && this.creep.memory.target === undefined) {
-      if(this.room.controller && this.room.controller.my && !_.values(Game.creeps).some(creep => creep.memory.role === 'carrier' && utils.samePos(creep.memory.owner, this.room.controller.pos))) {
-        if(this.room.memory.controller_container) {
-          var container = Game.getObjectById(this.room.memory.controller_container);
-          if(container) {
-            this.creep.memory.supplying = true;
-            this.creep.memory.owner = this.room.controller.pos
-            this.creep.memory.target = container.pos;
-          }
-        }
+      if(this.room.controller_container && !_.values(Game.creeps).some(creep => creep.memory.role === 'carrier' && creep.memory.target === this.room.controller_container)) {
+        this.creep.memory.supplying = true;
+        this.creep.memory.target = this.room.memory.controller_container;
       }
     }
     if(Memory.has_cpu && Memory.terminal !== undefined) {
       var terminal = Game.getObjectById(Memory.terminal);
       if(terminal.store[RESOURCE_ENERGY] < config.terminal_min_energy) {
         this.creep.memory.supplying = true;
-        this.creep.memory.owner = terminal.pos;
-        this.creep.memory.target = terminal.pos;
+        this.creep.memory.target = terminal.id;
       } else if(terminal.store[RESOURCE_ENERGY] > config.terminal_max_energy) {
         this.creep.memory.supplying = false;
-        this.creep.memory.owner = terminal.pos;
-        this.creep.memory.target = terminal.pos;
+        this.creep.memory.target = terminal.id;
       }
     }
     if(Memory.has_cpu && this.creep.memory.target === undefined) {
       var towers = this.room.towers().sort((a, b)=> a.energy > b.energy ? 1 : -1);
       if(towers.length > 0) {
         this.creep.memory.supplying = true;
-        this.creep.memory.owner = towers[0].pos;
-        this.creep.memory.target = towers[0].pos;
+        this.creep.memory.target = towers[0].id;
       } else {
         this.creep.say('no target');
       }
@@ -110,7 +94,7 @@ Carrier.prototype.act = function() {
     } else if (this.creep.memory.mode === 'unload') {
       var trg;
       if(this.creep.memory.supplying) {
-        trg = Game.rooms[this.creep.memory.target.roomName].lookForAt(LOOK_STRUCTURES, this.creep.memory.target.x, this.creep.memory.target.y).filter(st => st.structureType === STRUCTURE_CONTAINER || st.structureType === STRUCTURE_TOWER || st.structureType === STRUCTURE_TERMINAL)[0];
+        trg = Game.getObjectById(this.creep.memory.target);
       } else {
         trg = this.room.getEnergySink(this.creep);
       }
@@ -118,15 +102,10 @@ Carrier.prototype.act = function() {
       if(this.creep.pos.isNearTo(trg)) {
         this.creep.transfer(trg, RESOURCE_ENERGY);
       } else {
-        var carriers = this.creep.pos.findInRange(FIND_MY_CREEPS).filter(creep => creep.memory.role === 'carrier' && creep.memory.supplying && creep.carry.energy < creep.carryCapacity);
-        if(carriers.length > 0) {
-          this.creep.transfer(carriers[0], RESOURCE_ENERGY);
-        }
         this.creep.goTo(trg);
       }
       if(this.creep.memory.supplying && this.creep.carry.energy === 0) {
         this.creep.memory.supplying = undefined;
-        this.creep.memory.owner = undefined;
         this.creep.memory.target = undefined;
         this.creep.memory.mode = 'load';
       }
